5b73fc6af423a615152227bfe275bb143854852b
broring <brunobroring@gmail.com>
2021-08-19 13:44:07 -0300

    primeiro commit

---
+++ Iniciante/meu primeiro programa.py
@@ -0,0 +1,11 @@
+print('*****My first Program*****')
+
+print('Bem vindo ao programa do Exercito!!')
+idade = int(input('Digite sua idade: '))
+peso = float(input('Digite seu Peso: '))
+altura = float(input('Digite sua Altura: '))
+
+if idade >= 18 and peso >= 60 and altura >= 1.70:
+    print('Você está apto para entrar no exercito !!!')
+else:
+    print('Você não está apto para entrar no exercito :/')
